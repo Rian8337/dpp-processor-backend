6df0972f97f6496ce77aab0a73e382f3d3e26d4d
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-05-25 21:02:19 +0800

    Read online replay instead to reduce network I/O

--- src/utils/replayBackendManager.ts
+++ src/utils/replayBackendManager.ts
@@ -1,26 +1,32 @@
+import { readFile } from "fs/promises";
+import { homedir } from "os";
+import { join } from "path";
+
 /**
  * The base URL of the replay backend.
  */
 export const baseURL = new URL("http://127.0.0.1:3005");
 
+const replayDirectory = join(
+    homedir(),
+    "..",
+    "..",
+    "DroidData",
+    "osudroid",
+    "zip",
+    "upload"
+);
+
 /**
- * Gets an online replay file from the replay backend.
+ * Gets an online replay file.
  *
  * @param scoreId The ID of the score associated with the replay.
- * @returns The replay file, `null` if not found or the request to the server failed.
+ * @returns The replay file, `null` if not found.
  */
-export async function getOnlineReplay(
+export function getOnlineReplay(
     scoreId: string | number
 ): Promise<Buffer | null> {
-    const url = new URL(`${baseURL}/get-online-replay`);
-    url.searchParams.set("scoreId", scoreId.toString());
-
-    const res = await fetch(url).catch(() => null);
-    if (!res) {
-        return null;
-    }
-
-    return Buffer.from(await res.arrayBuffer());
+    return readFile(join(replayDirectory, `${scoreId}.odr`)).catch(() => null);
 }
 
 /**

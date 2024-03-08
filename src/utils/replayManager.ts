75c53ac0a57c9cbb56eee5234954930ae4989e80
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-03-08 23:47:23 +0700

    Fix get online replay requesting over network rather than reading from local

--- src/utils/replayManager.ts
+++ src/utils/replayManager.ts
@@ -264,10 +264,9 @@ export function deleteUnprocessedReplay(path: string): Promise<void> {
 export function getOnlineReplay(
     scoreId: string | number
 ): Promise<Buffer | null> {
-    return fetch(`https://osudroid.moe/api/upload/${scoreId}.odr`)
-        .then((r) => r.arrayBuffer())
-        .then((r) => Buffer.from(r))
-        .catch(() => null);
+    return readFile(join(onlineReplayDirectory, `${scoreId}.odr`)).catch(
+        () => null
+    );
 }
 
 /**

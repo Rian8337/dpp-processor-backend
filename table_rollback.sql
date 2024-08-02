f96ad94465a3bb044d7a94204e6eea1ca7554e55
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-08-02 09:38:23 +0700

    Add beatmap cache population table to rollback script

--- table_rollback.sql
+++ table_rollback.sql
@@ -4,3 +4,4 @@ DROP TABLE IF EXISTS rebalance_droid_difficulty_attributes;
 DROP TABLE IF EXISTS live_osu_difficulty_attributes;
 DROP TABLE IF EXISTS rebalance_osu_difficulty_attributes;
 DROP TABLE IF EXISTS replay_transfer;
+DROP TABLE IF EXISTS beatmap_cache_population;

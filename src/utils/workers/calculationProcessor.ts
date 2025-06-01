480f1fc1858139f38958e8f2f382ff2d12a531f8
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-06-01 12:32:59 +0800

    Remove beatmap game mode workaround

--- src/utils/workers/calculationProcessor.ts
+++ src/utils/workers/calculationProcessor.ts
@@ -92,18 +92,11 @@ parentPort?.on("message", async (data: CalculationWorkerData) => {
         data;
 
     const beatmapMD5 = createHash("md5").update(data.beatmapFile).digest("hex");
+    const beatmap =
+        beatmapCache.get(beatmapMD5) ??
+        new BeatmapDecoder().decode(data.beatmapFile, gamemode).result;
 
-    let beatmap = beatmapCache.get(beatmapMD5);
-
-    if (!beatmap) {
-        beatmap = new BeatmapDecoder().decode(
-            data.beatmapFile,
-            gamemode,
-        ).result;
-
-        // Temporary because this is bugged - remove after updating module.
-        beatmap.mode = gamemode;
-
+    if (!beatmapCache.has(beatmapMD5)) {
         beatmapCache.set(beatmapMD5, beatmap);
     }
 

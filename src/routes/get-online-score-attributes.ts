0fbcc30d8a1fad056a3adf470afac24a0599b63b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-07-20 20:45:10 +0800

    Add replay validity check for online score attributes

--- src/routes/get-online-score-attributes.ts
+++ src/routes/get-online-score-attributes.ts
@@ -1,5 +1,14 @@
-import { Accuracy, MathUtils, Modes, ModUtil } from "@rian8337/osu-base";
-import { ReplayAnalyzer } from "@rian8337/osu-droid-replay-analyzer";
+import {
+    Accuracy,
+    MathUtils,
+    Modes,
+    ModUtil,
+    ScoreRank,
+} from "@rian8337/osu-base";
+import {
+    ReplayAnalyzer,
+    ReplayData,
+} from "@rian8337/osu-droid-replay-analyzer";
 import { Score } from "@rian8337/osu-droid-utilities";
 import { Router } from "express";
 import {
@@ -18,6 +27,61 @@ import { BeatmapOsuDifficultyCalculator } from "../utils/calculator/BeatmapOsuDi
 import { PerformanceCalculationParameters } from "../utils/calculator/PerformanceCalculationParameters";
 import { getOfficialBestReplay, getOnlineReplay } from "../utils/replayManager";
 import { validateGETInternalKey } from "../utils/util";
+import { OfficialDatabaseScore } from "../database/official/schema/OfficialDatabaseScore";
+import { isDeepStrictEqual } from "util";
+
+function isReplayValid(
+    databaseScore: Score | OfficialDatabaseScore,
+    replayData: ReplayData,
+): boolean {
+    // Wrap the score in a Score object.
+    const score =
+        databaseScore instanceof Score
+            ? databaseScore
+            : new Score({
+                  ...databaseScore,
+                  username: "",
+                  mark: databaseScore.mark as ScoreRank,
+                  date: databaseScore.date.getTime(),
+                  slider_tick_hit: databaseScore.sliderTickHit,
+                  slider_end_hit: databaseScore.sliderEndHit,
+              });
+
+    // For replay v1 and v2, there is not that much information - just check the accuracy and hash.
+    if (
+        score.hash !== replayData.hash ||
+        !score.accuracy.equals(replayData.accuracy) ||
+        // Also check if the accuracy is "empty", as in there are no hits at all.
+        Number.isNaN(replayData.accuracy.value())
+    ) {
+        return false;
+    }
+
+    // Replay v3 has way more information - compare all relevant ones.
+    if (replayData.isReplayV3()) {
+        if (
+            score.score !== replayData.score ||
+            score.combo !== replayData.maxCombo ||
+            (!(databaseScore instanceof Score) &&
+                (databaseScore.geki !== replayData.hit300k ||
+                    databaseScore.katu !== replayData.hit100k)) ||
+            score.rank !== replayData.rank
+        ) {
+            return false;
+        }
+
+        // Mods are compared later as they are more costly.
+        const scoreMods = score.mods.serializeMods();
+        const replayMods = replayData.convertedMods.serializeMods();
+
+        if (!isDeepStrictEqual(scoreMods, replayMods)) {
+            return false;
+        }
+    }
+
+    // Replay v4? Well... nothing new to check there, so let's end it here.
+    return true;
+}
 
 const router = Router();
 
@@ -92,8 +156,11 @@ router.get<
 
     let overrideParameters: PerformanceCalculationParameters | undefined;
 
-    if (data !== null && data.replayVersion < 3) {
-        // Old replay version - fill in missing data.
+    if (
+        data !== null &&
+        (data.replayVersion < 3 || !isReplayValid(score, data))
+    ) {
+        // Old replay version or invalid replay - fill in missing data.
         if (score instanceof Score) {
             overrideParameters = new PerformanceCalculationParameters({
                 accuracy: score.accuracy,

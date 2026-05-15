a5d073c46cc65431c1bf7818b59362b933a42d67
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-05-15 23:51:42 +0800

    Remove `AccountTransferCollectionManager`
    Unused.

--- src/database/managers/AliceDBCollection.ts
+++ src/database/managers/AliceDBCollection.ts
@@ -1,16 +1,10 @@
 import { Db } from "mongodb";
 import { RecentPlaysCollectionManager } from "./aliceDb/RecentPlaysCollectionManager";
-import { AccountTransferCollectionManager } from "./aliceDb/AccountTransferCollectionManager";
 
 /**
  * Contains collections from Alice DB.
  */
 export class AliceDBCollection {
-    /**
-     * The database collection for account transfers.
-     */
-    readonly accountTransfer: AccountTransferCollectionManager;
-
     /**
      * The database collection for recent plays.
      */
@@ -20,10 +14,6 @@ export class AliceDBCollection {
      * @param aliceDb The database that is only used by this bot (my database).
      */
     constructor(aliceDb: Db) {
-        this.accountTransfer = new AccountTransferCollectionManager(
-            aliceDb.collection("accounttransfer"),
-        );
-
         this.recentPlays = new RecentPlaysCollectionManager(
             aliceDb.collection("recentplays"),
         );

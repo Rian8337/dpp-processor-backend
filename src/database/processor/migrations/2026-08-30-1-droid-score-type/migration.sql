eb160458f9c2335412d5fa3065f46b857e15f5cd
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-08-30 23:47:00 +0800

    Fix wrong query
    This is PostgreSQL, not MySQL...

--- src/database/processor/migrations/2026-08-30-1-droid-score-type/migration.sql
+++ src/database/processor/migrations/2026-08-30-1-droid-score-type/migration.sql
@@ -1,2 +1,2 @@
-ALTER TABLE live_droid_difficulty_attributes MODIFY COLUMN maximum_score bigint NOT NULL;
-ALTER TABLE rebalance_droid_difficulty_attributes MODIFY COLUMN maximum_score bigint NOT NULL;
+ALTER TABLE live_droid_difficulty_attributes ALTER COLUMN maximum_score TYPE bigint;
+ALTER TABLE rebalance_droid_difficulty_attributes ALTER COLUMN maximum_score TYPE bigint;
